22680dcb2797854524c88b1c9341be94bf4b6242
Makoto Sugahara <30372121+mi-os@users.noreply.github.com>
2021-12-16 22:59:35 +0900

    add new file

---
+++ src/literal.ts
@@ -0,0 +1,12 @@
+export {};
+
+let dayOfTheWeek: 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat' = 'Sun';
+dayOfTheWeek = 'Fri';
+// dayOfTheWeek = 'Day';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+month = 12;
+// month = 13;
+
+let TRUE: true = true;
+// TRUE = false;
