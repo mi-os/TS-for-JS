aea540b9faab64b4eec77cac7bd703ef63d8ed57
Makoto Sugahara <30372121+mi-os@users.noreply.github.com>
2021-12-21 21:53:39 +0900

    add new file

---
+++ src/optional-arguments.ts
@@ -0,0 +1,17 @@
+export {};
+
+let bmi: (height: number, weight: number, printable?: boolean) => number = (
+  height: number,
+  weight: number,
+  printable?: boolean
+): number => {
+  const bmi: number = weight / (height * height);
+
+  if (printable) {
+    console.log({ bmi });
+  }
+  return bmi;
+};
+
+// bmi(height, weight, output or not)
+bmi(1.78, 74, true);
