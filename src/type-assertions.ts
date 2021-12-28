464999e18b1994818f1380a3ab864a4ed80e8a3e
Makoto Sugahara <30372121+mi-os@users.noreply.github.com>
2021-12-29 03:24:33 +0900

    add new file

---
+++ src/type-assertions.ts
@@ -0,0 +1,11 @@
+export {};
+
+let name = 'Tom';
+
+// let length = name.length as number;
+let length = (name as string).length;
+
+// Deprecated because it resembles JSX notation
+// let length = (<string>name).length;
+
+// length = 'foo';
